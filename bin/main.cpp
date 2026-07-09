f930c5d9024083794fa74351074856e0a5c605b7
Deinfifif999 <frinedldldld9@outlook.com>
2026-07-09 19:51:35 +0300

    Add main.cpp with variable declarations
    Initial implementation of main.cpp with variable declarations.

---
+++ bin/main.cpp
@@ -0,0 +1,13 @@
+
+
+
+
+
+int coint = 25;                       // Whole numbers (integers)
+
+
+double price = 19.99;               // Floating-point numbers (decimals)
+string name = "Alice";              // Text data
+
+bool isCodingFun = true;            // Boolean values (true or false)
+Console.WriteLine($"Hello {name},"); 
